37213c9aff22e6ec96d3bfbc4a6b205a21a1025a
MarteCodes <jeanmarte93@gmail.com>
2021-10-24 23:16:41 -0400

    demoday

---
+++ public/weather.js
@@ -0,0 +1,20 @@
+const weatherApi = () => {
+    
+    fetch('http://api.weatherapi.com/v1/current.json?key=19645e2074124ba1b17144809211810&q=01701&aqi=no')
+    .then(res => res.json())
+    .then(data => {
+        let icon = document.getElementById("weatherIcon")
+        let text = document.getElementById("weatherText")
+        let location = document.getElementById("weatherLocation")
+        let temp = document.getElementById("weatherTemp")
+          
+        icon.src = data.current.condition.icon
+        text.textContent = data.current.condition.text
+        location.textContent = `${data.location.name}, ${data.location.region}`
+        temp.textContent = `${Math.ceil(data.current.temp_f)}\u2109`
+       
+    })
+    .catch(err => console.log(err))
+}
+weatherApi()
+
