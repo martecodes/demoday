7c238b9e9e7dc24457c502f6cc6bbb7dcf16aee3
MarteCodes <jeanmarte93@gmail.com>
2021-10-27 13:17:41 -0400

    adding minor details

--- README.md
+++
@@ -1,2 +0,0 @@
-# demoday
-bus tracker
---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'url' : 'mongodb+srv://Yoda:Password@cluster0.jt19a.mongodb.net/mybus?retryWrites=true&w=majority',
+    'dbName' : 'demoday'
+}
